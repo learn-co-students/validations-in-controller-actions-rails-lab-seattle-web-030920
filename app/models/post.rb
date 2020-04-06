d3cbfa05a9aec959b1822859aff8c22b6411282a
jeff_simon <jeffsbillinfo@gmail.com>
2020-04-05 22:24:23 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"], message: "Invalid category"}
+    validates :content, length: {minimum: 100}
 end
